7c25fa5744a97fb2b8271062640c83f48f2352a8
unknown <vishnudinesh128@gmail.com>
2026-07-17 18:55:34 +0530

    solve the issue

--- worker/src/db.ts
+++ worker/src/db.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from "./generated/prisma";
+import { PrismaClient } from "./generated/prisma/client.js";
 import {PrismaPg} from "@prisma/adapter-Pg"
 
 const adapter=new PrismaPg({
